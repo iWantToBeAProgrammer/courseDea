b6265d22b795f5925e23612d60b28c0420dd2a96
iWantToBeAProgrammer <juanstevenson10@gmail.com>
2022-10-10 00:48:14 +0700

    navbar and js

--- tweak.js
+++ tweak.js
@@ -8,12 +8,13 @@ const productBox3 = document.getElementById('productBox3')
 const productBox4 = document.getElementById('productBox4')
 const productBox5 = document.getElementById('productBox5')
 const productBox6 = document.getElementById('productBox6')
+const back = document.getElementById('back');
 
 function getDetail(nomor) {
     console.log('produk : ', nomor)
     newBox.style.display = "block"
     wrapper.style.display = "none"
-
+    back.style.display = "block"
     if(nomor === 1) {
         productBox1.style.display = "flex";
     } else if (nomor === 2) {
@@ -26,6 +27,20 @@ function getDetail(nomor) {
         productBox5.style.display = "flex";
     } else if (nomor === 6) {
         productBox6.style.display = "flex";
-    }
+    }   
 }
 
+function goBack() {
+    window.location.reload();
+}
+
+window.onscroll = function() {
+    const header = document.querySelector ('#navBar');
+    const fixedNav = header.offsetTop;
+
+    if(window.pageYOffset > fixedNav) {
+        header.classList.add('navbar-fixed');
+    } else {
+        header.classList.remove('navbar-fixed');
+    }
+}
